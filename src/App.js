13ebaf833343722c7a4619502c94f34d91060cb3
Onkar Srivastava <onkarsrivastava@Onkars-MacBook-Pro.local>
2024-10-06 12:20:42 +0530

    frontend of the moneytracker website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className='basic'>
+        <input type='text' placeholder={'+200 new samsung TV'}/>
+        <input type='datetime-local'/>
+        </div>
+        <div>
+        <input type='text' placeholder={'description'}/>
+        </div>
+        <button type='submit'>Add new transaction</button>
+      </form>
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New samasung TV</div>
+            <div className='description'>time for new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price red'>- $500</div>
+            <div className='datetime'>2024-10-06</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New gig,website</div>
+            <div className='description'>time for new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price green'>+ $500</div>
+            <div className='datetime'>2024-10-06</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>New Iphone</div>
+            <div className='description'>time for new TV</div>
+          </div>
+          <div className='right'>
+            <div className='price red'>- $900</div>
+            <div className='datetime'>2024-10-06</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
